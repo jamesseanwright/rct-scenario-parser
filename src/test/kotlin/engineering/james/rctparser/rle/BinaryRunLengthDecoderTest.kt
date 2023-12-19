ff12a7280296c923633dacfbc522af50943e9101
James Wright <jsw@mahoganyteeth.com>
2023-12-19 21:51:39 +0000

    Simplifies test byte array initialisation to avoid conversion nastiness

--- src/test/kotlin/engineering/james/rctparser/rle/BinaryRunLengthDecoderTest.kt
+++ src/test/kotlin/engineering/james/rctparser/rle/BinaryRunLengthDecoderTest.kt
@@ -10,37 +10,28 @@ class BinaryRunLengthDecoderTest {
         fun decodeRunLengthEncodedBytes() {
                 // Example taken from https://freerct.github.io/RCTTechDepot-Archive/RLE.html
                 val encoded =
-                                arrayOf(
-                                                                0x00,
-                                                                0x47,
-                                                                0xFF,
-                                                                0x6F,
-                                                                0x05,
-                                                                0x64,
-                                                                0x20,
-                                                                0x6A,
-                                                                0x6F,
-                                                                0x62,
-                                                                0x21
-                                                )
-                                                .map { x -> x.toByte() }
-                                                .toByteArray()
+                                byteArrayOf(
+                                                0x00,
+                                                0x47,
+                                                -0x1,
+                                                0x6F,
+                                                0x05,
+                                                0x64,
+                                                0x20,
+                                                0x6A,
+                                                0x6F,
+                                                0x62,
+                                                0x21
+                                )
 
-                val expected =
-                                arrayOf(0x47, 0x6F, 0x6F, 0x64, 0x20, 0x6A, 0x6F, 0x62, 0x21)
-                                                .map { x -> x.toByte() }
-                                                .toByteArray()
+                val expected = byteArrayOf(0x47, 0x6F, 0x6F, 0x64, 0x20, 0x6A, 0x6F, 0x62, 0x21)
 
                 val expectedData = "Good job!"
 
                 val actual = decodeBinary(encoded)
 
                 assertFalse(actual.isFailure)
-                assertContentEquals(expected, actual.getOrDefault(arrayOf<Byte>().toByteArray()))
-
-                assertEquals(
-                                expectedData,
-                                actual.getOrDefault(arrayOf<Byte>().toByteArray()).decodeToString()
-                )
+                assertContentEquals(expected, actual.getOrDefault(byteArrayOf()))
+                assertEquals(expectedData, actual.getOrDefault(byteArrayOf()).decodeToString())
         }
 }
