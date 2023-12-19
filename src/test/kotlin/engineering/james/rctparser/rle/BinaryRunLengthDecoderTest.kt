7496ce9a3588bc0bde92b77851eb56cdeeaed5c1
James Wright <jsw@mahoganyteeth.com>
2023-12-19 21:53:07 +0000

    Formats decodeBinary() tests

--- src/test/kotlin/engineering/james/rctparser/rle/BinaryRunLengthDecoderTest.kt
+++ src/test/kotlin/engineering/james/rctparser/rle/BinaryRunLengthDecoderTest.kt
@@ -6,32 +6,16 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 
 class BinaryRunLengthDecoderTest {
-        @Test
-        fun decodeRunLengthEncodedBytes() {
-                // Example taken from https://freerct.github.io/RCTTechDepot-Archive/RLE.html
-                val encoded =
-                                byteArrayOf(
-                                                0x00,
-                                                0x47,
-                                                -0x1,
-                                                0x6F,
-                                                0x05,
-                                                0x64,
-                                                0x20,
-                                                0x6A,
-                                                0x6F,
-                                                0x62,
-                                                0x21
-                                )
+    @Test
+    fun decodeRunLengthEncodedBytes() {
+        // Example taken from https://freerct.github.io/RCTTechDepot-Archive/RLE.html
+        val encoded = byteArrayOf(0x00, 0x47, -0x1, 0x6F, 0x05, 0x64, 0x20, 0x6A, 0x6F, 0x62, 0x21)
+        val expected = byteArrayOf(0x47, 0x6F, 0x6F, 0x64, 0x20, 0x6A, 0x6F, 0x62, 0x21)
+        val expectedData = "Good job!"
+        val actual = decodeBinary(encoded)
 
-                val expected = byteArrayOf(0x47, 0x6F, 0x6F, 0x64, 0x20, 0x6A, 0x6F, 0x62, 0x21)
-
-                val expectedData = "Good job!"
-
-                val actual = decodeBinary(encoded)
-
-                assertFalse(actual.isFailure)
-                assertContentEquals(expected, actual.getOrDefault(byteArrayOf()))
-                assertEquals(expectedData, actual.getOrDefault(byteArrayOf()).decodeToString())
-        }
+        assertFalse(actual.isFailure)
+        assertContentEquals(expected, actual.getOrDefault(byteArrayOf()))
+        assertEquals(expectedData, actual.getOrDefault(byteArrayOf()).decodeToString())
+    }
 }
