10d5661f5d33ccec0be16f985ab3229e0c1531f9
James Wright <jsw@mahoganyteeth.com>
2023-12-23 20:12:14 +0000

    Reverts RLD to imperative iteration-based implementation
    See comment above the decodeBinary() signature for an explanation

--- src/main/kotlin/engineering/james/rctparser/rle/BinaryRunLengthDecoder.kt
+++ src/main/kotlin/engineering/james/rctparser/rle/BinaryRunLengthDecoder.kt
@@ -2,22 +2,29 @@ package engineering.james.rctparser.rle
 
 val checksumLength = 4
 
+// I had attempted to write this more functionally, but
+// it proved to be orders of magnitude slower, to the
+// extent at which it'd take over a minute to parse a
+// 300KB file!
 fun decodeBinary(encoded: ByteArray): ByteArray {
-    tailrec fun iterate(acc: List<Byte> = listOf<Byte>(), i: Int = 0): List<Byte> {
-        when {
-            i == encoded.size - checksumLength -> return acc
-            isEncodedRun(encoded[i]) -> {
-                val n = 1 - encoded[i].toInt()
-                return iterate(acc + List(n) { encoded[i + 1] }, i + 2)
-            }
-            else -> {
-                val n = i + encoded[i].toInt() + 1
-                return iterate(acc + encoded.slice(i + 1..n), n + 1)
-            }
+    val out = mutableListOf<Byte>()
+    var i = 0
+
+    while (i < encoded.size - checksumLength) {
+        val isEncodedRun = encoded[i] < 0 // i.e. a signed byte with 1 as MSB
+
+        if (isEncodedRun) {
+            val n = 1 - encoded[i].toInt()
+            out.addAll(ByteArray(n) { encoded[i + 1] }.asList())
+            i += 2
+        } else {
+            val n = i + encoded[i].toInt() + 1
+            out.addAll(encoded.slice(i + 1..n))
+            i = 1 + n
         }
     }
 
-    return iterate().toByteArray()
+    return out.toByteArray()
 }
 
 fun isEncodedRun(byte: Byte) = byte < 0 // i.e. MSB is 1
